67068571f306b737ca345216db6e209cdd3f13bc
Rick Bradley <rick@rickbradley.com>
2011-04-26 13:52:22 -0500

    starting form work; using padrino-helpers
    took a bit of reorganizing to just get padrino-helpers in and happy.

--- pledgenothing.rb
+++ pledgenothing.rb
@@ -2,9 +2,23 @@ require 'rubygems'
 require 'isolate/now'
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'sinatra/base'
+require 'padrino-helpers'
 require 'haml'
 
-get '/' do
-  haml :index
+class PledgeNothing < Sinatra::Base
+  register Padrino::Helpers
+
+  get '/' do
+    haml :index
+  end
+
+  get '/pledges/new' do
+    haml :new_pledge, :layout => false
+  end
+
+  post '/pledges/create' do
+    raise "Shit"
+  end
 end
 
