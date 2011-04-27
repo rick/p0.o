d4f20501ff7e51656bc5e2393e081b6320fb7047
Rick Bradley <rick@rickbradley.com>
2011-04-27 16:18:13 -0500

    Allow for anonymous pledges

--- public/javascripts/pledgenothing.js
+++ public/javascripts/pledgenothing.js
@@ -10,13 +10,15 @@ jQuery(document).ready(function($) {
     var class_year   = $("select[name='class_year']").val();
     var comments     = $("textarea[name='comments']").val();
     var email        = $(".new-pledge-form input[name='email']").val();
+    var is_anonymous = $(".new-pledge-form input[name='is_anonymous']").is(':checked');
     
     $("#facebox .popup .content").load("/create", { 
       "name"         : name, 
       "pledge_years" : pledge_years, 
       "class_year"   : class_year,
       "comments"     : comments,
-      "email"        : email
+      "email"        : email,
+      "is_anonymous" : is_anonymous
     });
     
     return false;
